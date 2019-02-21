8321aa1a547a8d4637a9e5ed4bb3203bc20e3112
sarahces <sarahces005@gmail.com>
2019-02-21 09:48:49 -0500

    Lab 2

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -13,9 +13,10 @@ test -d ~/Pictures || mkdir ~/Pictures
 
 # download a zipfile of pictures to our Pictures directory if it isn't already there - assumes you are online
 test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net/pics.zip
-
+test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
 # unpack the downloaded zipfile if it is there, then delete the zipfile
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
+test -f ~/Pictures/pics.tgz && tar -xvzf ~/Pictures/pics.tgz -C ~/Pictures/ && rm ~/Pictures/pics.tgz
 
 # Make a report on what we have in the Pictures directory
 test -d ~/Pictures && cat <<EOF
