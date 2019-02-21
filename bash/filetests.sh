0529f59e0b7d835c4f3964533c6a37af3d4eea7c
sarahces <sarahces005@gmail.com>
2019-02-21 15:55:21 -0500

    Lab 3

--- bash/filetests.sh
+++ bash/filetests.sh
@@ -2,6 +2,7 @@
 # This script demonstrates file testing
 
 # Test for the existence of the /etc/resolv.conf file
+[ -x /etc/resolv.conf ] && echo "/etc/resolv.conf is executable" || echo "/etc/resolv.conf is not executable"
 # TASK 1: Add a test to see if the /etc/resolv.conf file is a regular file
 [ -f /etc/resolv.conf ] && echo "/etc/resolv.conf is a regular file" || echo "/etc/resolv.conf is not a regular file"
 # TASK 2: Add a test to see if the /etc/resolv.conf file is a symbolic link
@@ -13,15 +14,15 @@
  # TASK 5: Add a test to see if the /etc/resolv.conf file is writable
 [ -w /etc/resolv.conf ] && echo "/etc/resolv.conf is writable" || echo "/etc/resolv.conf is not writable"
 # TASK 6: Add a test to see if the /etc/resolv.conf file is executable
-[ -e /etc/resolv.conf ] && echo "/etc/resolv.conf exists" || echo "/etc/resolv.conf does not exist"
+[ -x /etc/resolv.conf ] && echo "/etc/resolv.conf is executable" || echo "/etc/resolv.conf is not executable"
 # Tests if /tmp is a directory
+[ -d /tmp ] && echo "/tmp is a directory" || echo "/tmp is not a directory"
 # TASK 4: Add a test to see if the /tmp directory is readable
 [ -r /tmp ] && echo "/tmp is a readable directory" || echo "/tmp is not a readable directory"
 # TASK 5: Add a test to see if the /tmp directory is writable
 [ -w /tmp ] && echo "/tmp is a writable directory" || echo "/tmp is not a writable directory"
 # TASK 6: Add a test to see if the /tmp directory can be accessed
-[ -d /tmp ] && echo "/tmp is a directory" || echo "/tmp is not a directory"
-
+[ -x /tmp ] && echo "/tmp is a accessible directory" || echo "/tmp is not a accessible directory"
 # Tests if one file is newer than another
 # TASK 7: Add testing to print out which file newest, or if they are the same age
 [ /etc/hosts -nt /etc/resolv.conf ] && echo "/etc/hosts is newer than /etc/resolv.conf"
